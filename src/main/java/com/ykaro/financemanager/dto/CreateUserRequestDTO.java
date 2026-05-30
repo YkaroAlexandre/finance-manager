f07f7d3fc47047bcc4b38eb4b8568a1967dbd6d1
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-05-30 10:00:21 -0300

    feat: criação do DTO para User.

--- src/main/java/com/ykaro/financemanager/dto/CreateUserRequestDTO.java
+++ src/main/java/com/ykaro/financemanager/dto/CreateUserRequestDTO.java
@@ -1,12 +1,18 @@
 package com.ykaro.financemanager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateUserRequestDTO {
+    @NotBlank
     private String name;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private String password;
 }
--- src/main/java/com/ykaro/financemanager/dto/UserResponseDTO.java
+++ src/main/java/com/ykaro/financemanager/dto/UserResponseDTO.java
@@ -1,13 +1,16 @@
 package com.ykaro.financemanager.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
-@Setter
+@Builder
 public class UserResponseDTO {
     private Long id;
     private String name;
     private String email;
-    private String created_at;
+    private LocalDateTime created_at;
 }
