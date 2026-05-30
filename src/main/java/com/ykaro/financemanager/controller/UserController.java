5ee3556c9ae6673a3bf67d7c6deb711fbf87180f
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-05-30 09:58:48 -0300

    refactor: alterando para ser utilizado o DTO

--- src/main/java/com/ykaro/financemanager/controller/UserController.java
+++ src/main/java/com/ykaro/financemanager/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ykaro.financemanager.controller;
 
+import com.ykaro.financemanager.dto.CreateUserRequestDTO;
+import com.ykaro.financemanager.dto.UserResponseDTO;
 import com.ykaro.financemanager.entity.UserEntity;
 import com.ykaro.financemanager.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserEntity createUser(@RequestBody UserEntity user) {
-        return userService.createUser(user);
+    public UserResponseDTO createUser(@RequestBody CreateUserRequestDTO dto){
+        return userService.createUser(dto);
     }
 
     @GetMapping
