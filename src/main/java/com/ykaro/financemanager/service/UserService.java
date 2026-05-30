96df7011cab22fc9c71cc229d661582218ea9c87
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-05-30 10:01:01 -0300

    refactor: alterado para utilizar o DTO

--- src/main/java/com/ykaro/financemanager/service/UserService.java
+++ src/main/java/com/ykaro/financemanager/service/UserService.java
@@ -1,9 +1,11 @@
 package com.ykaro.financemanager.service;
 
+import com.ykaro.financemanager.dto.CreateUserRequestDTO;
+import com.ykaro.financemanager.dto.UserResponseDTO;
 import com.ykaro.financemanager.entity.UserEntity;
 import com.ykaro.financemanager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,5 +16,26 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
-    
+
+    public UserResponseDTO createUser(CreateUserRequestDTO dto) {
+        UserEntity user = UserEntity.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .created_at(LocalDateTime.now())
+                .build();
+
+        UserEntity savedUser = userRepository.save(user);
+
+        return UserResponseDTO.builder()
+                .id(savedUser.getId())
+                .name(savedUser.getName())
+                .email(savedUser.getEmail())
+                .created_at(savedUser.getCreated_at())
+                .build();
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
