5603ca173de0f3d2a28a315422316b6b20071ea2
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-06-11 07:39:33 -0300

    fix: Alterando retorno para o DTO

--- src/main/java/com/ykaro/financemanager/controller/UserController.java
+++ src/main/java/com/ykaro/financemanager/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping
-    public List<UserEntity> getAllUsers() {
+    public List<UserResponseDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
