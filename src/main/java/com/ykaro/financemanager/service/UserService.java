d4039bd85ac2bac4c45720d426a3f531c4b3ede6
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-06-15 20:43:19 -0300

    fix: Pequenos ajustes para diminuir duplicidade de códigos, adicionando as exceções

--- src/main/java/com/ykaro/financemanager/service/UserService.java
+++ src/main/java/com/ykaro/financemanager/service/UserService.java
@@ -4,12 +4,13 @@ import com.ykaro.financemanager.dto.CreateUserRequestDTO;
 import com.ykaro.financemanager.dto.UpdateUserRequestDTO;
 import com.ykaro.financemanager.dto.UserResponseDTO;
 import com.ykaro.financemanager.entity.UserEntity;
+import com.ykaro.financemanager.exception.ResourceNotFoundException;
 import com.ykaro.financemanager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,45 +18,7 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
-
-    public UserResponseDTO createUser(@NonNull CreateUserRequestDTO dto) {
-        UserEntity user = UserEntity.builder()
-                .name(dto.getName())
-                .email(dto.getEmail())
-                .password(dto.getPassword())
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        UserEntity savedUser = userRepository.save(user);
-
-        return UserResponseDTO.builder()
-                .id(savedUser.getId())
-                .name(savedUser.getName())
-                .email(savedUser.getEmail())
-                .createdAt(savedUser.getCreatedAt())
-                .build();
-    }
-
-    public List<UserResponseDTO> getAllUsers() {
-        List<UserEntity> savedUsers = userRepository.findAll();
-        List<UserResponseDTO> users = new java.util.ArrayList<>();
-        for (UserEntity user : savedUsers) {
-            UserResponseDTO userToSave =UserResponseDTO.builder()
-                    .id(user.getId())
-                    .name(user.getName())
-                    .email(user.getEmail())
-                    .createdAt(user.getCreatedAt())
-                    .build();
-            users.add(userToSave);
-        }
-        return users;
-    }
-
-    public UserResponseDTO getUserById(Long id) {
-
-        UserEntity user = userRepository.findById(id)
-                .orElseThrow();
-
+    private UserResponseDTO toResponseDTO(UserEntity user) {
         return UserResponseDTO.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -64,15 +27,47 @@ public class UserService {
                 .build();
     }
 
+
+    public UserResponseDTO createUser(CreateUserRequestDTO dto) {
+        UserEntity user = UserEntity.builder()
+                .name(dto.getName())
+                .email(dto.getEmail().strip().toLowerCase())
+                .password(dto.getPassword())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        UserEntity savedUser = userRepository.save(user);
+
+        return toResponseDTO(savedUser);
+    }
+
+    public List<UserResponseDTO> getAllUsers() {
+        List<UserEntity> savedUsers = userRepository.findAll();
+        List<UserResponseDTO> users = new ArrayList<>();
+
+        for (UserEntity user : savedUsers) {
+            users.add(toResponseDTO(user));
+        }
+
+        return users;
+    }
+
+    public UserResponseDTO getUserById(Long id) {
+
+        UserEntity user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado!"));
+
+        return toResponseDTO(user);
+    }
+
     public void deleteUserById(Long id) {
         UserEntity user = userRepository.findById(id)
-                .orElseThrow();
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado!"));
         userRepository.delete(user);
     }
 
     public UserResponseDTO updateUserById(UpdateUserRequestDTO dto, Long id) {
         UserEntity user = userRepository.findById(id)
-                .orElseThrow();
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado!"));
         if (dto.getName() != null && !dto.getName().isBlank()){
             user.setName(dto.getName());
         }
@@ -81,11 +76,6 @@ public class UserService {
         }
         UserEntity savedUser = userRepository.save(user);
 
-        return UserResponseDTO.builder()
-                .id(savedUser.getId())
-                .name(savedUser.getName())
-                .email(savedUser.getEmail())
-                .createdAt(savedUser.getCreatedAt())
-                .build();
+        return toResponseDTO(savedUser);
     }
 }
