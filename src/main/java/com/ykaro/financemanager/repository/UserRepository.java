e4bf498d83e036500f53ab6fa43f9b4b42e95ca0
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-06-20 11:02:57 -0300

    feat: Adicionando funcionalidade de buscar por Email

--- src/main/java/com/ykaro/financemanager/repository/UserRepository.java
+++ src/main/java/com/ykaro/financemanager/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.ykaro.financemanager.repository;
 import com.ykaro.financemanager.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
