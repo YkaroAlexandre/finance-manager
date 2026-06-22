bd86e8b90aade6488e9541adcd541a6ded3b123a
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-06-22 07:44:27 -0300

    feat: implementando endpoint de login

--- src/main/java/com/ykaro/financemanager/controller/AuthController.java
+++ src/main/java/com/ykaro/financemanager/controller/AuthController.java
@@ -1,6 +1,23 @@
 package com.ykaro.financemanager.controller;
 
-// Falta implementar
-public class AuthController {
+import com.ykaro.financemanager.dto.LoginRequestDTO;
+import com.ykaro.financemanager.dto.LoginResponseDTO;
+import com.ykaro.financemanager.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@Valid @RequestBody LoginRequestDTO dto) {
+        return authService.login(dto);
+    }
 }
