a16e8aabb854f6342046ef88f65a8e500e32805d
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-06-27 09:34:41 -0300

    feat: Adicionando funcionalidades de gerar e atutenticar token, ainda em implementeção

--- src/main/java/com/ykaro/financemanager/security/JwtService.java
+++ src/main/java/com/ykaro/financemanager/security/JwtService.java
@@ -1,21 +1,58 @@
 package com.ykaro.financemanager.security;
 
+import com.ykaro.financemanager.exception.InvalidCredentialsException;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.time.LocalDateTime;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
     @Value("${jwt.secret-key}")
     private String secretKey;
-    private SecretKey secretKeyObj = Keys.hmacShaKeyFor(secretKey.getBytes());
-//    private String jwtExpiration = String.valueOf((LocalDateTime.now().getMinute() +40));
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String email) {
-        return null;
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(expiration, ChronoUnit.MILLIS)))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try{
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        }
+        catch (Exception e){
+            throw new InvalidCredentialsException("Credenciais Inválidas");
+        }
     }
 }
