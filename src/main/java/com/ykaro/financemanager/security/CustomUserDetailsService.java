a83c848efa15fde711ed66095bf64fa265ad68f7
Ykaro Alexandre <ykaroalexandre@hotmail.com>
2026-07-01 20:25:58 -0300

    feat: adicionando função de segurança CustomUserDatailsService

---
+++ src/main/java/com/ykaro/financemanager/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ykaro.financemanager.security;
+
+import com.ykaro.financemanager.entity.UserEntity;
+import com.ykaro.financemanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+
+        return user;
+    }
+}
